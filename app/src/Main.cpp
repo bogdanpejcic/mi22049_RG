3399db8cfc54d1d7f977df59f629f480bd3402d3
bogi <bogdanpejcic@gmail.com>
2025-07-05 21:25:51 +0200

    black window, app setup

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,9 +1,12 @@
 #include <engine/core/Engine.hpp>
 
+#include "MyApp.hpp"
+
 /**
  * Start here...
 test commit......
  */
-int main(int argc, char** argv) {
-    return 0;
+int main(int argc, char **argv) {
+    auto app = std::make_unique<app::MyApp>();
+    return app->run(argc, argv);
 }
---
+++ app/src/MyApp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by bogi on 7/5/25.
+//
+#include "../include/MyApp.hpp"
+#include <spdlog/spdlog.h>
+
+namespace app {
+    void MyApp::app_setup() {
+        spdlog::info("App setup completed!");
+    }
+} // app
