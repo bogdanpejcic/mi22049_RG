5d3687fdbdc6ffe26cf6a71a053f51723e3f2b18
bogi <bogdanpejcic@gmail.com>
2025-07-12 12:42:57 +0200

    impelented basic GUI

---
+++ app/src/GuiController.cpp
@@ -0,0 +1,39 @@
+//
+// Created by bogi on 7/12/25.
+//
+
+#include "../include/GuiController.hpp"
+
+#include "imgui.h"
+#include "engine/graphics/GraphicsController.hpp"
+#include "engine/platform/PlatformController.hpp"
+
+namespace app {
+    void GuiController::initialize() {
+        set_enable(false);
+    }
+
+    std::string_view GuiController::name() const {
+        return "app::GUIController";
+    }
+
+    void GuiController::poll_events() {
+        auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+        if (platform->key(engine::platform::KEY_F2).state() == engine::platform::Key::State::JustPressed) {
+            set_enable(!is_enabled());
+        }
+    }
+
+    void GuiController::draw() {
+        auto graphics = engine::core::Controller::get<engine::graphics::GraphicsController>();
+        auto camera = graphics->camera();
+        graphics->begin_gui();
+
+        ImGui::Begin("Camera info");
+        ImGui::Text("Camera position: (%f, %f, %f)", camera->Position.x, camera->Position.y, camera->Position.z);
+
+        ImGui::End();;
+
+        graphics->end_gui();
+    }
+} // app
--- app/src/MyApp.cpp
+++ app/src/MyApp.cpp
@@ -4,12 +4,15 @@
 #include "../include/MyApp.hpp"
 #include <spdlog/spdlog.h>
 
+#include "GuiController.hpp"
 #include "MainController.hpp"
 
 namespace app {
     void MyApp::app_setup() {
         spdlog::info("App setup completed!");
         auto main_controller = register_controller<app::MainController>();
+        auto gui_controller = register_controller<app::GuiController>();
         main_controller->after(engine::core::Controller::get<engine::core::EngineControllersEnd>());
+        main_controller->before(gui_controller);
     }
 } // app
