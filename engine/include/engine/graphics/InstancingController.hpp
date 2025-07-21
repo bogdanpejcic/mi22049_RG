58db2e4afebfe3ef53b5eb354492bbe641483988
bogi <bogdanpejcic@gmail.com>
2025-07-22 01:36:32 +0200

    Implement instancing into engine InstancingController

--- engine/include/engine/graphics/InstancingController.hpp
+++ engine/include/engine/graphics/InstancingController.hpp
@@ -5,15 +5,29 @@
 #ifndef INSTANCINGCONTROLLER_HPP
 #define INSTANCINGCONTROLLER_HPP
 #include "engine/core/Controller.hpp"
+#include "engine/resources/Model.hpp"
+#include "glm/fwd.hpp"
 
 namespace engine::graphics {
     class InstancingController final : public core::Controller {
     public:
         std::string_view name() const override;
 
-    private:
-        void initialize() override;
-    };
-} // engine
+        unsigned int amount;
+        glm::mat4 *model_matrices;
+        float radius;
+        float offset;
+        unsigned int buffer;
 
+        void generate_model_transformation_matrices();
+
+        void configure_instanced_array();
+
+        void set_transformation_matrices(resources::Model *model);
+
+        void configure_transformation_matrices(resources::Shader *shader);
+
+        void draw_instances(const resources::Shader *shader, const resources::Model *model);
+    }; // engine
+}
 #endif //INSTANCINGCONTROLLER_HPP
